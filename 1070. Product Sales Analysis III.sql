61f4d167eadc05265b59a2c77854c00c726d4d21
Shreena Mani <167695614+ItsShreena@users.noreply.github.com>
2026-07-13 10:14:53 +0530

    Implement product sales analysis query
    Added a MySQL query to analyze product sales by year and quantity.

---
+++ 1070. Product Sales Analysis III.sql
@@ -0,0 +1,65 @@
+Table: Sales
+
++-------------+-------+
+| Column Name | Type  |
++-------------+-------+
+| sale_id     | int   |
+| product_id  | int   |
+| year        | int   |
+| quantity    | int   |
+| price       | int   |
++-------------+-------+
+(sale_id, year) is the primary key (combination of columns with unique values) of this table.
+Each row records a sale of a product in a given year.
+A product may have multiple sales entries in the same year.
+Note that the per-unit price.
+
+Write a solution to find all sales that occurred in the first year each product was sold.
+
+For each product_id, identify the earliest year it appears in the Sales table.
+
+Return all sales entries for that product in that year.
+
+Return a table with the following columns: product_id, first_year, quantity, and price.
+Return the result in any order.
+
+ 
+
+Example 1:
+
+Input: 
+Sales table:
++---------+------------+------+----------+-------+
+| sale_id | product_id | year | quantity | price |
++---------+------------+------+----------+-------+ 
+| 1       | 100        | 2008 | 10       | 5000  |
+| 2       | 100        | 2009 | 12       | 5000  |
+| 7       | 200        | 2011 | 15       | 9000  |
++---------+------------+------+----------+-------+
+
+Output: 
++------------+------------+----------+-------+
+| product_id | first_year | quantity | price |
++------------+------------+----------+-------+ 
+| 100        | 2008       | 10       | 5000  |
+| 200        | 2011       | 15       | 9000  |
++------------+------------+----------+-------+
+
+
+ANSWER:
+
+# Write your MySQL query statement be
+SELECT s.product_id,
+s.year AS first_year,
+s.quantity,
+s.price#displaying all the columns needed and made converting by AS which are needed
+FROM Sales s#using the table sales
+JOIN(#making a temporary subtable/subquery which doesnt exist 
+   SELECT
+    product_id,
+    MIN(year) AS first_year
+    FROM Sales
+    GROUP BY product_id#getting grouped by product id by putting all rows same as product id
+)f#here f is a name given to the temporary madeup table
+ON s.product_id=f.product_id#match the same table
+AND s.year=f.first_year;

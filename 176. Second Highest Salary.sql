b27dc2e4c29419f217031c2cd77ad491ddb182dc
Shreena Mani <167695614+ItsShreena@users.noreply.github.com>
2026-07-26 17:00:13 +0530

    Implement query for second highest salary
    Added SQL query to find the second highest distinct salary from the Employee table.

---
+++ 176. Second Highest Salary.sql
@@ -0,0 +1,63 @@
+Table: Employee
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| id          | int  |
+| salary      | int  |
++-------------+------+
+id is the primary key (column with unique values) for this table.
+Each row of this table contains information about the salary of an employee.
+ 
+
+Write a solution to find the second highest distinct salary from the Employee table. If there is no second highest salary, return null (return None in Pandas).
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Employee table:
++----+--------+
+| id | salary |
++----+--------+
+| 1  | 100    |
+| 2  | 200    |
+| 3  | 300    |
++----+--------+
+Output: 
++---------------------+
+| SecondHighestSalary |
++---------------------+
+| 200                 |
++---------------------+
+Example 2:
+
+Input: 
+Employee table:
++----+--------+
+| id | salary |
++----+--------+
+| 1  | 100    |
++----+--------+
+Output: 
++---------------------+
+| SecondHighestSalary |
++---------------------+
+| null                |
++---------------------+
+
+
+ANSWER:
+
+# Write your MySQL query statement below
+SELECT(#Were selecting the result of a subquery.The subquery returns one value.
+    SELECT DISTINCT salary #distinct removes duplicates
+    FROM Employee#table
+    ORDER BY salary DESC# seeing salary we order it decreasing
+    LIMIT 1 OFFSET 1##skip 1st row...OFFSET whereas LIMIT is take 1 row
+
+)AS SecondHighestSalary;#name it as this
+
